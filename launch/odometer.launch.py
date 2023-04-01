062d8b1f9704c38ebea062690286c4f15798beb1
Christopher Coballes <cobecoballes@gmail.com>
2023-04-02 05:47:15 +0800

    Update odometer.launch.py

--- launch/odometer.launch.py
+++ launch/odometer.launch.py
@@ -1,1 +1,11 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
 
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_control',
+            executable='odometer',
+            output='screen'
+        )
+    ])
