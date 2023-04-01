d715f19e37a46e30c6a21e78a3a3090fdd6fffe2
Christopher Coballes <cobecoballes@gmail.com>
2023-04-02 05:00:10 +0800

    Update robot_description.launch.py

--- launch/robot_description.launch.py
+++ launch/robot_description.launch.py
@@ -1,1 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    robot_description_path = os.path.join(
+        get_package_share_directory('robot_control'),
+        'urdf',
+        'robot.urdf'
+    )
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(robot_description_path).read()}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(get_package_share_directory('robot_control'), 'rviz', 'robot.rviz')]
+        )
+    ])
 
