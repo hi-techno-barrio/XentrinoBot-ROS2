39aebd3d1b384ed1afc608518bf972030be69588
Christopher Coballes <cobecoballes@gmail.com>
2023-04-02 05:57:14 +0800

    Update lidar.launch.py

--- launch/lidar.launch.py
+++ launch/lidar.launch.py
@@ -1,2 +1,13 @@
 
-ok
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_control',
+            executable='lidar',
+            output='screen'
+        )
+    ])
+
