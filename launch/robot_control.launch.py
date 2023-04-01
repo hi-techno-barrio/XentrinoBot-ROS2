7945a7a57e259d587964466fe08db0d791106e10
Christopher Coballes <cobecoballes@gmail.com>
2023-04-02 05:59:28 +0800

    Update robot_control.launch.py

--- launch/robot_control.launch.py
+++ launch/robot_control.launch.py
@@ -1,1 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_description',
+            executable='robot_description',
+            output='screen'
+        )
+    ])
 
