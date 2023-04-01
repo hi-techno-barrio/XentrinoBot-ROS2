fb300928329081caccf921acf566d648bbc995a8
Christopher Coballes <cobecoballes@gmail.com>
2023-04-02 05:46:27 +0800

    Update imu.launch.py

--- launch/imu.launch.py
+++ launch/imu.launch.py
@@ -1,1 +1,12 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_control',
+            executable='imu',
+            output='screen'
+        )
+    ])
 
